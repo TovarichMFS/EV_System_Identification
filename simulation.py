86730a936e0cd7122d35a75faa12af77af4ed9a7
Abdul-Razak Adam <a4lph4@gmail.com>
2018-06-08 13:38:13 +0300

    random states generation work-in-progress

--- simulation.py
+++ simulation.py
@@ -5,13 +5,16 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import keyboard
 import numpy as np
+import random
 
 lr = 0.5  # distance from CoM to rear wheels
 lf = 0.5  # distance from CoM to front wheels
 
 dt = 0.1  # timestep
 
-N = 300  # simulate for 1000 timesteps
+N = 100  # simulate for 1000 timesteps
+
+w = 5 # size of window
 
 def f_prime(state, a, delta_f):
     x   = state[0]
@@ -50,47 +53,71 @@ def euler(state, state_dot, dt):
 def main():
     # define initial state
     state = [0, 0, 0, 0] 
+    random_state = [0, 0, 0, 0] 
     x = []
     y = []
+
+    random_x = []
+    random_y = []
 
     a = .1
     delta_f = .0
 
     states = [state]
-
+    random_states = [random_state]
     # simulate for N steps with constant controls
     for i in range(N):
-        time.sleep(0.1)
-        if keyboard.is_pressed('q'):
-            print('left')
-            delta_f += 0.0001
-        elif keyboard.is_pressed('z'):
-            print('up')
-            a += 0.01
-        elif keyboard.is_pressed('d'):
-            print('right')
-            delta_f -= 0.0001
-        elif keyboard.is_pressed('s'):
-            print('down')
-            a -= 0.01
-        else:
-            if delta_f > 0.0:
-                delta_f -= 0.0001
-            elif delta_f < 0.0:
-                delta_f += 0.0001
-        # get derivative
-        state_dot = f_prime(state, a, delta_f)
 
-        # integrate
-        # state = state + state_dot*dt
-        x.append(state[0])
-        y.append(state[1])
-        state = euler(state, state_dot, dt)
-        print(state)
-        states.append(state)
+        if i % w == 0: # if window
+            random_state = [random_state[0], random_state[1],random.uniform(-state[2],state[2]), random.uniform(-state[3],state[3])]
+        else:
+            time.sleep(0.1)
+            if keyboard.is_pressed('q'):
+                print('left')
+                delta_f += 0.0001
+            elif keyboard.is_pressed('z'):
+                print('up')
+                a += 0.01
+            elif keyboard.is_pressed('d'):
+                print('right')
+                delta_f -= 0.0001
+            elif keyboard.is_pressed('s'):
+                print('down')
+                a -= 0.01
+            else:
+                if delta_f > 0.0:
+                    delta_f -= 0.0001
+                elif delta_f < 0.0:
+                    delta_f += 0.0001
+
+            # get derivative
+            state_dot = f_prime(state, a, delta_f)
+            random_state_dot = f_prime(random_state, random.uniform(-a,a), random.uniform(-delta_f, delta_f))
+
+            # integrate
+            # state = state + state_dot*dt
+            x.append(state[0])
+            y.append(state[1])
+            random_x.append(random_state[0])
+            random_y.append(random_state[1])
+
+            state = euler(state, state_dot, dt)
+
+            print(random_state)
+            random_state = euler(random_state,random_state_dot, dt)
+            #print(state)
+            print(random_state)
+            print('......................')
+            states.append(state)
+            random_states.append(random_state)
+            
     plt.plot(x,y)
     plt.savefig('myfig')
-    plt.show()
+    plt.close()
+
+    plt.plot(random_x,random_y)
+    plt.savefig('myfig_random')
+    # plt.show()
 
     # print resultng trajectory
     # TODO: add visualization
@@ -100,7 +127,7 @@ def main():
 
 def window_stack(array, stepsize, width):
     n = array.shape[0]
-    return np.hstack( array[i:1+n+i-width:stepsize] for i in range(0,width) )
+    return np.hstack( array[i:1+n+i-width:stepsize] for i in range(0,width))
 
 def last_window(array, size):
     return array[-size:]
